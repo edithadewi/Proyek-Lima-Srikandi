d8594c3ab782266574c482a015c22aa28c675a05
Alifmiftahul <36036406+Alifmiftahul@users.noreply.github.com>
2018-04-02 13:45:02 +0700

    Alif 2

--- app/src/main/java/limasrikandi/editha/example/com/limasrikandi/DetailKlinik.java
+++ app/src/main/java/limasrikandi/editha/example/com/limasrikandi/DetailKlinik.java
@@ -5,9 +5,27 @@ import android.os.Bundle;
 
 public class DetailKlinik extends AppCompatActivity {
 
+    TextView alamat_klinik, no_tel;
+    ImageView logo_klinik;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_klinik);
+    Intent intent = getIntent(); // mengambil data dari activity sebeleumnya
+        String jalan = intent.getStringExtra("jalan");
+        Integer telp = intent.getIntExtra("telp",0);
+        Integer photo = intent.getIntExtra("photo",0);
+
+        Log.d("getIntExtra", ""+telp+photo);
+
+        alamat_klinik= (TextView)findViewById(R.id.alamat);
+        no_tel = (TextView)findViewById(R.id.no_hp);
+        logo_klinik = (ImageView)findViewById(R.id.logo);
+
+        alamat_klinik.setText(jalan);
+        no_tel.setText(telp);
+        logo_klinik.setImageResource(photo);
     }
 }
+
