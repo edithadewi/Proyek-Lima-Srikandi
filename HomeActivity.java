ba57241fa0c00a8c520b5b02b86b0590c9fdf176
edithadewi <36092290+edithadewi@users.noreply.github.com>
2018-04-20 18:21:15 +0700

    hometr

---
+++ HomeActivity.java
@@ -0,0 +1,25 @@
+package limasrikandi.editha.example.com.limasrikandi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void DaftarKlinik(View view) {
+        Intent intent = new Intent(HomeActivity.this,DaftarKlinik.class);
+        startActivity(intent);
+    }
+
+    public void gotoHomeTreatment(View view) {
+        Intent intent = new Intent(HomeActivity.this, HomeTreatment.class);
+        startActivity(intent);
+    }
+}
